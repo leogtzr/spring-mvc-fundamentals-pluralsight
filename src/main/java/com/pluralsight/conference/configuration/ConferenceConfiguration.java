f74c307bdf7629ab7843ba0f49dbc721e078da73
Leo Gutierrez <Leonardo.Gutierrez@autozone.com>
2022-02-12 23:12:13 -0700

    add locale

--- src/main/java/com/pluralsight/conference/configuration/ConferenceConfiguration.java
+++ src/main/java/com/pluralsight/conference/configuration/ConferenceConfiguration.java
@@ -1,9 +1,16 @@
 package com.pluralsight.conference.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
@@ -14,6 +21,27 @@ public class ConferenceConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("/files/**")
                 .addResourceLocations("classpath:/files/")
                 ;
+    }
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        final SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.US);
+
+        return sessionLocaleResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        final LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+
+        return localeChangeInterceptor;
     }
 
 }
