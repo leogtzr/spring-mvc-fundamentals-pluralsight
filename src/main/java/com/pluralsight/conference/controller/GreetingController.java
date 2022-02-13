5dd54dbbac48c7348aadef22d36f8a6fba1974be
Leo Gutierrez <Leonardo.Gutierrez@autozone.com>
2022-02-12 20:00:28 -0700

    add code to cls

---
+++ src/main/java/com/pluralsight/conference/controller/GreetingController.java
@@ -0,0 +1,18 @@
+package com.pluralsight.conference.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("greeting")
+    public String greeting(final Map<String, Object> model) {
+        model.put("message", "Hello, Leo");
+
+        return "greeting";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/greeting.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+	<h1>Greeting</h1>
+	<p>${greeting}</p>
+
+</body>
+</html>
