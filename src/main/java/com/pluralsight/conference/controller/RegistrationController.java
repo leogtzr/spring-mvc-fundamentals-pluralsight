4b6cf23825012a074fa868edc3cc6fe72e3d93a9
Leo Gutierrez <Leonardo.Gutierrez@autozone.com>
2022-02-12 21:47:09 -0700

    change to POST http method

--- src/main/java/com/pluralsight/conference/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/conference/controller/RegistrationController.java
@@ -6,17 +6,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegistrationController {
 
     private static final Logger log = LoggerFactory.getLogger(RegistrationController.class);
 
-    @GetMapping("registration")
+    @GetMapping("register")
+    public String register() {
+
+        return "registration";
+    }
+
+    @PostMapping("registration")
     public String registration(final @ModelAttribute("registration") Registration registration) {
         log.info("Got: {}", registration);
 
-        return "registration";
+        return "index";
     }
 
 }
